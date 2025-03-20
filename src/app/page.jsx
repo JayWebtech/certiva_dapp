4f2b49dfebdffc5a47eca0b8793363b4f86f0353
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2025-03-20 16:52:33 +0100

    feat: build the failed verification display for all screen responsivness

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-white"> Home page </h1>
+      <h1 className="text-white"> 
+        Home page
+      </h1>
     </div>
   );
 }
