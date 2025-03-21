2e956bd6f12e68c2208d42dbd61f0bcfe65a9928
Emmanuel <emanuelanyigor@gmail.com>
2025-03-21 13:25:41 +0100

    working on the navbar and hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,19 @@
+"use client";
+import Navbar from "@/components/layout/Navbar";
 import Faq from "../components/home/Faq";
 import Footer from "../components/layout/Footer";
-"use client";
 
 import Hero from "@/components/home/Hero";
-import Navbar from "@/components/layout/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <div className="hero-section h-screen w-full">
+        <Navbar />
+        <Hero />
+      </div>
       <Faq />
       <Footer />
-      <Navbar />
-      <Hero />
-      <h1 className="text-white"> Home page </h1>
     </div>
   );
 }
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { Button } from "../form";
 
 const Hero = () => {
     return (
-        <div className="hero text-white">
-            <div className="container mx-auto">
-                <div className="flex items-center justify-center">
+        <div className="text-white">
+            <div className="mx-auto px-4 sm:px-10 md:px-8 lg:px-16">
+                <div className="flex items-center justify-center mt-[5em]">
                     <div className="hero-content text-center py-[64px]">
-                        <h1 className="text-[36px] font-[800] max-w-[724px] mx-auto"> Revolutionizing <span className="text-primary">Academic Credentials</span> with Blockchain Technology. </h1>
-                        <p> Unichain ensures instant, secure academic credential verification using Starknet and Zero-Knowledge Proofs. </p>
+                        <h1 className="text-[1.5em] lg:text-[2.5em] text-center leading-none font-bold"> Revolutionizing <span className="text-primary">Academic Credentials</span> <br /> with Blockchain Technology. </h1>
+                        <p className="my-3 max-w-xl mx-auto text-center text-sm lg:text-lg"> Unichain ensures instant, secure academic credential verification using Starknet and Zero-Knowledge Proofs. </p>
                         <div className="hero-cta">
-                            <div className="flex items-center justify-center gap-4">
-                               
+                            <div className="flex items-center justify-center gap-4 mt-4 max-w-[480px] mx-auto">
+                               <Button 
+                                    title="For universityies"
+                                    variant="primary"
+                                    className="rounded-[133.333px] py-[16px]"
+                               />
+
+                               <Button 
+                                    title="For employers"
+                                    variant="white"
+                                    className="rounded-[133.333px] py-[16px]"
+                               />
                             </div>
                         </div>
                     </div>
