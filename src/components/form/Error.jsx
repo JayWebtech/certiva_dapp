89ac1e9d4f0db7f3fa0cbfc8731a4a22ef53b32e
JayWebtech <jethroadamzy@gmail.com>
2025-04-24 18:56:27 +0100

    refac : convert project to typescript

--- src/components/form/Error.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Error = ({ error, visible }) => {
-  if (!visible) return null;
-  return <p className="text-sm text-error"> {error} </p>;
-};
-
-export default Error;
